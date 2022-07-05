5e8d2e8ccf8e9a877578dd350121ae8f9d903f01
uxconor <conor.ux@gmail.com>
2022-07-05 15:02:34 -0400

    connected the GraphQL server. Functional now

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,15 @@
-import { ApolloServer } from "apollo-server";
-import typeDefs from "./schema.js";
-// const typeDefs = require("./schema");
+import { ApolloServer } from 'apollo-server';
+import typeDefs from './schema.js';
+import { resolvers } from './resolvers';
+import { TrackAPI } from './datasources/track-api.js';
 
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(6)]
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    trackAPI: new TrackAPI(),
   }),
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
-
-const server = new ApolloServer({ typeDefs, mocks });
+});
 
 server.listen().then(() => {
   console.log(`
