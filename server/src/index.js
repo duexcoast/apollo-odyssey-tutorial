89663bc20b0472c4d38709d18221e752a0624d25
uxconor <conor.ux@gmail.com>
2022-07-05 15:43:23 -0400

    fixed resolvers import on server. ES module needed .js

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema.js';
-import { resolvers } from './resolvers';
+import { resolvers } from './resolvers.js';
 import { TrackAPI } from './datasources/track-api.js';
 
 const server = new ApolloServer({
